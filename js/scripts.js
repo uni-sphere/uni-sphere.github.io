651536c78a69a7632c4265cbe9cf8cc563c1475b
Gabriel Muller <gabriel.muller.12@gmail.com>
2016-01-21 16:54:06 +0000

    final

--- js/scripts.js
+++ js/scripts.js
@@ -104,11 +104,6 @@
       var name = $("input#name").val();
       var email = $("input#email").val();
       var message = $("textarea#message").val();
-      var firstName = name; // For Success/Failure Message
-      // Check for white space in name for Success/Fail message
-      if (firstName.indexOf(' ') >= 0) {
-        firstName = name.split(' ').slice(0, -1).join(' ');
-      }
       $.ajax({
         success: function() {
           Rollbar.info("Contact request",{email: email, name: name, message: message});
@@ -123,17 +118,7 @@
 
           //clear all fields
           $('#contactForm').trigger("reset");
-        },
-        error: function() {
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
+        }
       })
     },
     filter: function() {
